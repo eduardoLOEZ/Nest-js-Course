3d28c0db9757ac48f138d013df2ad91bb3166884
Eduardo Loeza <eduardoloeza@Eduardos-MacBook-Air.local>
2024-05-10 15:06:09 -0600

    avances del curso

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+      }),
+    }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
+    /*TypeOrmModule.forRoot({
       type: 'postgres', // type of our database
       host: 'localhost', // database host
       port: 5432, // database host
@@ -16,6 +24,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: 'postgres', // name of our database,
       autoLoadEntities: true, // models will be loaded automatically
       synchronize: true, // your entities will be synced with the database(recommended: disable in production)
+    }),
+    */
+
+    /* AppModule updated to use process.env variables */
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT, //si agregamos un +, lo pasa a un number para que no de error
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
